8fcc06ff6d2b920b864dd51248a763b7d167b27e
chauthanhbinh19 <21521871@gm.uit.edu.vn>
2024-04-29 12:54:02 +0700

    Fix UI

---
+++ app/src/main/java/com/example/scarlet/OrderDetailsActivity.java
@@ -0,0 +1,36 @@
+package com.example.scarlet;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    RelativeLayout back_btn;
+    private void BindView(){
+        back_btn=findViewById(R.id.back_btn);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activities_detail);
+
+        Window window = getWindow();
+        window.setNavigationBarColor(ContextCompat.getColor(this, R.color.burgundy));
+        
+        BindView();
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+}
